1ad9079af246585294f9ba6ee4d27179982bc781
Thomas <thomwill212@gmail.com>
2022-07-18 21:23:20 +0200

    Full loading + minor bugfixes

--- thirty_dollar_ui/loader.py
+++ thirty_dollar_ui/loader.py
@@ -6,14 +6,36 @@ import os
 
 # DATA PATHS
 #
-# Images:
+# Images: data / images / type / id
+# Sounds: data / audio / id
 #
+# REAL PATHS
+#
+# Images: ./data/assets/sound/id.png
+# Sounds: ./data/audio/id.wav
+# Fonts: ./data/fonts
 
 
 def init(data: dict):
     # Preparing up the terrain
     data["data"] = {}
     data["data"]["images"] = {}
+    data["data"]["audio"] = {}
 
-    print("Loading images...")
-    data["data"]["images"]["pause"] = [pygame.image.load("")]
+    print("Loading assets...")
+
+    i = 2
+    for t in ["action", "sound"]:
+        for a in data["id"][t]:
+            data["data"]["images"][t][a] = pygame.image.load(f"data/assets/{t}/{a}.png")
+            i += 1
+    data["data"]["images"]["pause"] = [pygame.image.load("data/assets/empty.png")]
+    print(f"Loaded {i} assets.")
+
+    print("Loading sounds...")
+    i = 1
+    for s in data["id"]["sound"]:
+        data["data"]["audio"][s] = pygame.sound.load(f"data/audio/{s}.wav")
+        i += 1
+
+    print(f"Loaded {i} sounds.")
